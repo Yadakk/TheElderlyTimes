54ccb94fa48a35e584ca10fc438eb88a428e31a4
Yadakk <lolyer47@gmail.com>
2024-08-09 14:42:46 +0300

    0.13.2 Changed AbsoluteRelativePathExtension to utility

--- Assets/Project/Scripts/Library/UtilityMethods/Path/AbsoluteRelativePathUtility.cs
+++ Assets/Project/Scripts/Library/UtilityMethods/Path/AbsoluteRelativePathUtility.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public static class AbsoluteRelativePathExtension
+public static class AbsoluteRelativePathUtility
 {
-    public static string AbsoluteToRelativePath(this string path)
+    public static string AbsoluteToRelativePath(string path)
     {
         if (!path.StartsWith(Application.dataPath)) return path;
         path = "Assets" + path[Application.dataPath.Length..];
         return path;
     }
 
-    public static string RelativeToAbsolutePath(this string path)
+    public static string RelativeToAbsolutePath(string path)
     {
         if (!path.StartsWith("Assets")) return path;
         path = Path.GetDirectoryName(Path.GetFullPath(Path.Combine(Application.dataPath, @"../")) + path);
